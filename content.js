21c49e60ccbe12ae84265312d84851adf6cf3ee7
JDOdea <jondavidodea@gmail.com>
2025-03-11 11:57:52 -0500

    initialized content.js

---
+++ content.js
@@ -0,0 +1,15 @@
+chrome.storage.sync.get("userData", (data) => {
+    if (!data.userData) return;
+
+    const { name, email, phone } = data.userData;
+
+    document.querySelectorAll("input").forEach((input) => {
+        if (input.name.toLowerCase().includes("name")) {
+            input.value = name;
+        } else if (input.name.toLowerCase().includes("email")) {
+            input.value = email;
+        } else if (input.name.toLowerCase().includes("phone")) {
+            input.value = phone;
+        }
+    });
+});
